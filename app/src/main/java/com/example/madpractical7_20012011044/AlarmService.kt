3670ff911924d8fd84fc42c6a4dcacbc3ed814eb
kenil gor <kenil.a.patel.10@gmail.com>
2022-11-17 07:46:45 +0530

    Initial commit

---
+++ app/src/main/java/com/example/madpractical7_20012011044/AlarmService.kt
@@ -0,0 +1,25 @@
+package com.example.madpractical7_20012011044
+
+import android.app.Service
+import android.content.Intent
+import android.media.MediaPlayer
+import android.os.IBinder
+import madpractical7_20012011044.R
+
+class AlarmService : Service() {
+    var mp: MediaPlayer? = null
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        if(intent != null) {
+            mp = MediaPlayer.create(this, R.raw.alarm);
+            mp?.start()
+        }
+        return START_STICKY
+    }
+    override fun onDestroy() {
+        mp?.stop()
+        super.onDestroy()
+    }
+}
